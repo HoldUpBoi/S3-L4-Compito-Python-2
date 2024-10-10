7b39ba72b7b5a8346785956054aa3831ac5b9f0b
HoldUpBoi <daniele.meroni09@gmail.com>
2024-10-10 17:36:29 +0200

    Whoops, dimenticato gli altri :3

--- Esercizio Compito.py
+++ Esercizio Compito.py
@@ -1,25 +1,73 @@
+import math
 
-print("--- Programma calcolo perimetro rettangolo ---\n \n")
+print("------- Calcolatrice personale ------- \n")
 
-base = 0 
+print("Quale perimetro vuoi calcolare? \n")
 
-while base == 0:
-    base = int(input("Inserire la base del rettangolo da inserire: \n"))
-    if base == 0:
-        print("Il numero inserito è zero. Per favore, inserisci un numero diverso da zero.")
+while True:
+    try:
+        scelta = int(input("1) Quadrato\n2) Cerchio\n3) Rettangolo\n4) Esci \n"))
 
-altezza = 0
+        if 1 <= scelta <= 4:
+            break
+        else:
+            print("Scelta invalida, prova un altro numero. \n")
 
-while altezza == 0:
-    altezza = int(input("Inserire l'altezza del rettangolo da inserire: \n"))
-    if altezza == 0:
-        print("Il numero inserito è zero. Per favore, inserisci un numero diverso da zero.")
+    except ValueError:
+            print("Scelta invalida, per favore inserire un numero. \n")
 
-base = base * 2
+if (scelta == 1):
 
-altezza = altezza * 2
+    lato = 0 
 
-perimetro = base + altezza
+    while lato == 0:
+        lato = int(input("Inserire un lato del quadrato da inserire: \n"))
+        if lato == 0:
+            print("Il numero inserito è zero. Per favore, inserisci un numero diverso da zero. \n")
 
-print("Il perimetro del rettangolo inserito è: ", perimetro)
+    perimetro = lato * 4
+
+    print("Il perimetro del quadrato inserito è: ", perimetro)
+
+elif (scelta == 2):
+
+    raggio = 0 
+
+    while raggio == 0:
+        raggio = int(input("Inserire il raggio del cerchio da inserire: \n"))
+        if raggio == 0:
+            print("Il numero inserito è zero. Per favore, inserisci un numero diverso da zero. \n")
+
+    perimetro = 2 * math.pi * raggio
+
+    print("Il perimetro del cerchio inserito è", perimetro)
+
+elif (scelta ==3 ):
+
+
+    base = 0 
+
+    while base == 0:
+        base = int(input("Inserire la base del rettangolo da inserire: \n"))
+        if base == 0:
+            print("Il numero inserito è zero. Per favore, inserisci un numero diverso da zero. \n")
+
+    altezza = 0
+
+    while altezza == 0:
+        altezza = int(input("Inserire l'altezza del rettangolo da inserire: \n"))
+        if altezza == 0:
+            print("Il numero inserito è zero. Per favore, inserisci un numero diverso da zero. \n")
+
+    base = base * 2
+
+    altezza = altezza * 2
+
+    perimetro = base + altezza
+
+    print("Il perimetro del rettangolo inserito è: ", perimetro)
+
+elif (scelta == 4):
+
+    print("Arrivederci!")
 
